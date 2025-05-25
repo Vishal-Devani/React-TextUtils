83303a0f93ef5b006e35ff0be449b2253db1ea44
Vishal Devani <vishaldevani001@gmail.com>
2025-05-25 17:03:25 +0530

    Updates

---
+++ precache-manifest.766bc1c18cfa929b42e0cf7032533d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8f3f083827cbc3d54fb6fc7e8fe3f0",
+    "url": "/React-TextUtils/index.html"
+  },
+  {
+    "revision": "dfa1a4087fe63539ee3f",
+    "url": "/React-TextUtils/static/css/main.b73be6be.chunk.css"
+  },
+  {
+    "revision": "94eed275faeeddd8d3fe",
+    "url": "/React-TextUtils/static/js/2.045831d9.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/React-TextUtils/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "dfa1a4087fe63539ee3f",
+    "url": "/React-TextUtils/static/js/main.7d8f2b00.chunk.js"
+  },
+  {
+    "revision": "540e24e5f8004782b9df",
+    "url": "/React-TextUtils/static/js/runtime~main.15d0a490.js"
+  }
+]);
